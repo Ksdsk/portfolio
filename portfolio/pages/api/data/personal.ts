8ba09dd5a1400c80dff3dbe72468a79289767c89
Ksdsk <soonmoc.kang@gmail.com>
2023-09-27 02:00:23 -0300

    system message breakdown

--- portfolio/pages/api/data/personal.ts
+++ portfolio/pages/api/data/personal.ts
@@ -6,8 +6,8 @@ Aside from the resume, you should know a bit more about yourself:
     - Giving rubber ducks away to people (Total 144 rubber ducks given away so far)
 
 - Social media:
-    - 
-
+    - https://www.linkedin.com/in/smdanielkang
+    - https://github.com/Ksdsk
 `
 
 export default personal;
--- portfolio/pages/api/gpt.ts
+++ portfolio/pages/api/gpt.ts
@@ -2,6 +2,7 @@ import { ChatGPTAPI, ChatMessage } from "chatgpt";
 import 'dotenv/config'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import resume from "./data/resume";
+import personal from "./data/personal";
 
 const api = new ChatGPTAPI({
     apiKey: process.env.OPENAI_API_KEY!,
@@ -21,13 +22,13 @@ export default async function handler(
 
     if (req.body["historyId"] == "") {
         const comp = await api.sendMessage(req.body["prompt"], {
-            systemMessage: resume + "\n Use markdown, but tildes (~) should be escaped!",
+            systemMessage: resume + personal + "\n Use markdown, but tildes (~) should be escaped!",
             timeoutMs: 1000 * 60
         })
         res.status(200).json({ completion: comp })
     } else {
         const comp = await api.sendMessage(req.body["prompt"], {
-            systemMessage: resume + "\n Use markdown, but tildes (~) should be escaped!",
+            systemMessage: resume + personal + "\n Use markdown, but tildes (~) should be escaped!",
             parentMessageId: req.body["historyId"],
             timeoutMs: 1000 * 60
         })
